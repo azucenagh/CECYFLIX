58ff541d6838d97661a2521e820306cbdea5a804
azucenagh <sweet.azu.app@gmail.com>
2025-07-19 22:45:23 -0600

    actualizando el back3

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,7 @@ app.post('/api/recomendaciones', async (req, res) => {
 });
 
 // Conectar a MongoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('✅ Conectado a MongoDB Atlas'))
     .catch((err) => console.error('❌ Error al conectar a MongoDB:', err));
 
